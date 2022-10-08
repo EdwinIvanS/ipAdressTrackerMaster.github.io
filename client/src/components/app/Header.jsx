739098cc91c50a17beff972db9776a8a7bb2af99
EdwinIvanS <sedwin118@hotmail.com>
2022-10-07 19:39:44 -0500

    agregando diseño para el contenedor del Header

---
+++ client/src/components/app/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
+export function Header() {
+    return(
+        <React.Fragment>
+            <header className="header">
+                <Container className="container-header">
+                    <div className="title-ip">
+                        <h1>IP Adress Tracker</h1>
+                        <form>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit"> 
+                                <FontAwesomeIcon icon={faAngleDoubleRight} />
+                            </Button>
+                        </form>
+                    </div>
+                </Container>
+            </header>
+            
+        </React.Fragment>
+    )
+}
+
---
+++ server/controller/controllerIndex.js
@@ -0,0 +1,7 @@
+const controllerIndex  = {
+    index : function(req, res) {
+        res.render('index');
+    }
+}
+
+module.exports = controllerIndex;
